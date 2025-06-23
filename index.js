4f442d9a627393b1d685674500f7ace6c2f05dd2
bella234428 <ibseali258@gmail.com>
2025-06-23 13:27:12 +0300

    add code

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+// saturdayFun - function declaration with default parameter
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// mondayWork - function expression with default parameter
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+// wrapAdjective - returns a function (closure) with default parameter for flair and inner function parameter default
+function wrapAdjective(flair = "*") {
+  return function(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
